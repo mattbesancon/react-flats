521346da374ea30cfa071cb8db3d0b034ccefef4
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-04-10 16:26:26 +0200

    source for flat html

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,15 +3,16 @@ import React, { Component } from 'react';
 import flats from '././data/flats.js';
 
 const Flat = (props) => {
-  return (
-    <div class="card" style="background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(`${props.flats.imageUrl}`;);">
-    <div class="card-category"> {props.flats.price} EUR</div>
-    <div class="card-description">
-        <h2>{props.flats.price}</h2>
-    </div>
-    <a class="card-link" href="#"></a>
-    </div>
-  );
+    const background = `background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.props.flats.imageurl};);">`;
+    return (
+        <div class="card" style={background}></div>
+        <div class="card-category"> {props.flats.price} EUR</div>
+        <div class="card-description">
+            <h2>{props.flats.price}</h2>
+        </div>
+        <a class="card-link" href="#"></a>
+        </div>
+    );
 };
 
 export default Flat;
