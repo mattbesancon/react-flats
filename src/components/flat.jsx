42577b1472905364bc83c9406a19b1133d534708
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-04-11 17:39:35 +0200

    refactor flat & starting map component

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 import flats from '././data/flats.js';
 
 const Flat = (props) => {
-  const background = `background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.props.flats.imageurl};);">`;
+  const background = `background-image: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${this.flats[props.id].imageUrl};);">`;
   return (
-    <div class="card" style={background}>
-      <div class="card-category"> {this.props.flats.price} EUR</div>
-      <div class="card-description">
+    <div className="card" style={background}>
+      <div className="card-category"> {this.props.flats.price} EUR</div>
+      <div className="card-description">
         <h2>{this.props.flats.price}</h2>
       </div>
-      <a class="card-link" href="#"></a>
+      <a className="card-link" href="#"></a>
     </div>
   );
 };
--- src/components/google_map_react..jsx
+++ src/components/google_map_react..jsx
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
 
-class GoogleMapReact extends Component {
-  handleUpdate = (event) => {
-    this.props.searchFunction(event.target.value);
-  }
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 59.95,
+      lng: 30.33
+    },
+    zoom: 11
+  };
 
   render() {
     return (
-      <input
-        type="text"
-        className="form-control form-search"
-        onChange={this.handleUpdate}
-      />
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     );
   }
 }
 
-export default GoogleMapReact;
+export default SimpleMap;
