ae6f468709c5dca7aeaf035d3af178d02f67cef5
Matthieu Besan�con <matthieu.besancon@gmail.com>
2020-04-11 17:49:09 +0200

    first draft app.jsx

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 
-import GoogleMapReact from './google_mao_react';
+import SimpleMap from './simple_map';
 import Marker from './marker';
 import Flat from './flat';
 import FlatList from './flat_list';
@@ -18,15 +18,13 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <div className="left-scene">
-          <SearchBar searchFunction={this.search} />
-          <div className="selected-gif">
-            <Gif id={this.state.selectedGifId} />
-          </div>
-        </div>
-        <div className="right-scene">
-          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+      <div className="flat-list">
+        <FlatList />
+      </div>
+      <div className="map-container">
+        <SimpleMap />
+        <div className="marker"></div>
+          <Marker />
         </div>
       </div>
     );
